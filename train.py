3f099dd4576fda66c2765a6ae45b9243e651d672
Henry <chenhouyuan@gmail.com>
2018-07-20 01:26:47 +0800

    python3 print fix

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ data = getStockDataVec(stock_name)
 l = len(data) - 1
 batch_size = 32
 
-for e in xrange(episode_count + 1):
+for e in range(episode_count + 1):
 	print("Episode " + str(e) + "/" + str(episode_count))
 	state = getState(data, 0, window_size + 1)
 
